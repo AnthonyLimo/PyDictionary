62c81cfaa3142f122ff9a2316711d86d764a921e
Anthony Kiplimo <anthonylimo90@gmail.com>
2018-08-08 22:39:16 +0300

    Feature additions
    Added the ability to get suggested words and the option to retry word input

--- dict.py
+++ dict.py
@@ -7,9 +7,23 @@ def translate_word(define_word):
     define_word = define_word.lower()
     if define_word in data:
         return data[define_word]
+    elif len(get_close_matches(define_word, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y for yes and N for no" % get_close_matches(define_word, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(define_word, data.keys())[0]]
+        elif yn == "N":
+            return "We couldn't find your entry"
+        else:
+            return "The word doesn't exist in the entry"
     else:
         return "Sorry, this word cannot be found. Please double check it."
 
 word = input("Enter word to define: ")
 
-print(translate_word(word))
+output = translate_word(word)
+
+if type(output) == list:
+    for item in output:
+        print item
+else:
+    print(output)
